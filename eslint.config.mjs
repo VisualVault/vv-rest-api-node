d1a3057a21f875ed700b73f9c777de4c60b20ecd
Juan Vallejo <juan.vallejo@visualvault.com>
2026-06-17 13:07:35 -0300

    Add Linter and setup require-jsdoc as lint warning

---
+++ eslint.config.mjs
@@ -0,0 +1,41 @@
+import jsdocPlugin from 'eslint-plugin-jsdoc';
+
+export default [
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js'],
+    plugins: {
+      jsdoc: jsdocPlugin,
+    },
+    rules: {
+      // Require JSDoc for functions and classes
+      'jsdoc/require-jsdoc': [
+        'warn',
+        {
+          require: {
+            FunctionDeclaration: true,
+            MethodDefinition: true,
+            ClassDeclaration: true,
+            ArrowFunctionExpression: true,
+            FunctionExpression: true,
+          },
+        },
+      ],
+      // Catch missing parameters/return tags and bad types
+      'jsdoc/require-param': 'warn',
+      'jsdoc/require-returns': 'warn',
+      'jsdoc/check-types': 'warn',
+    },
+  },
+  // Prevent require-jsdoc from running on test and config files
+  {
+    files: [
+      "**/__tests__/**/*.[jt]s?(x)",
+      "**/?(*.)+(spec|test).[jt]s?(x)",
+      "**/tests/**/*.[jt]s?(x)",
+      "**/*.config.js"
+    ],
+    rules: {
+      "jsdoc/require-jsdoc": "off"
+    }
+  }
+];
